119195889d56b0223a485aaccc7f47ba828de34d
soupwithsawdust <133097281+soupwithsawdust@users.noreply.github.com>
2023-05-22 12:26:13 +1000

    Update animation.py

--- animation/animation.py
+++ animation/animation.py
@@ -4,14 +4,14 @@ import os
 def cls():
     os.system(['clear','cls'][os.name == 'nt'])
 
-f1 = open("frame1.txt")
-f2 = open("frame2.txt")
-f3 = open("frame3.txt")
-f = [f1,f2,f3]
+f11,f12,f13,f14,f15,f16,f17,f18 = open("Battle1.1.txt"),open("Battle1.2.txt"),open("Battle1.3.txt"),open("Battle1.4.txt"),open("Battle1.5.txt"),open("Battle1.6.txt")
+,open("Battle1.7.txt"),open("Battle1.8.txt")
+f1=[f11,f12,f13,f14,f15,f16,f17,f18]
 
-for k in f:
-  for i in k.readlines():
-    print(i)
-  time.sleep(1)
-  cls()
+def animation1(f1):
+    for k in f:
+      for i in k.readlines():
+        print(i)
+      time.sleep(1)
+      cls()
   
